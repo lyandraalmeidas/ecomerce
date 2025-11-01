f4a436c0f22be3625fa9d043c74711254dbf83ac
lyandra <lyandraalmeidadasilva99@gmail.com>
2025-10-31 22:13:53 -0300

    refazendo css

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Quindle</title>
+        <title>eBooksCloud</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.4/font/bootstrap-icons.css">
@@ -18,7 +18,7 @@
         <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="/">
-                    <img src="../IMGS/logo.png" alt="Logo da Livraria Virtual" width="100" height="50" class="d-inline-block align-text-top">
+                    <img src="../IMGS/logo.png" alt="Logo da Livraria Virtual" class="logo d-inline-block align-text-top">
                 </a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
@@ -28,7 +28,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <form class="d-flex search-form" role="search" action="#" method="get" aria-label="Formulário de busca">
                         <input class="form-control me-2" type="search" name="q" placeholder="Buscar livros, autores..." aria-label="Buscar">
-                        <button class="btn btn-outline-light" type="submit" aria-label="Enviar busca">
+                        <button class="btn search-btn" type="submit" aria-label="Enviar busca">
                             <i class="bi bi-search" aria-hidden="true"></i>
                         </button>
                     </form>
